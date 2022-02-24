cc2d4ccb8df36ddbb3959f2beaaf0e7e654b17c6
chris.egnatis@gmail.com <chris.egnatis@gmail.com>
2022-02-24 15:33:29 -0600

    changed data

--- src/Components/data.js
+++ src/Components/data.js
@@ -3,9 +3,9 @@ export const projectData = [
         
         title: 'NHL Stats App',
         date: '2/2022',
-        paragraph: 'Developed a web application to display real time statistics of the National Hockey League. Utilizes TypeScript, Tailwind CSS, and dynamic API calls to fetch current data, and error handling for API call misses. Best Practices implemented to create maintainable and reliable code. Website makes over one-thousand API calls.',
-        // paragraph: 'At UTD Hackreason 2022, an AI themed hackathon, my team created a finalist project called Resume Filter. Resume Filter is a program companies can use to automate their process of giving out interviews to the most qualified candidates. The project was coded in Prolog, with the help of s(CASP) in the command line. This hackathon was a challenge to me and my team because we were not experienced in logic programming languages. Scoring amongst 6 finalists was an accomplishment for my team.', 
-        hasYoutube: 'false',
+        paragraph: 'Developed a web application to display real time statistics of the NHL\'s teams and players in interactive tables. Utilizes TypeScript for type checking, Tailwind CSS for UI Design, and dynamic API calls using Axios. Implements React Table to implement sortable table and React Popup to make a clean UI.',
+        hasYoutube: 'true',
+        youtube: '9kAy26O464Y',
         hasGithub: 'true',
         githubSrc: 'https://github.com/chrisegnatis/NHLStatsApp'
 
@@ -27,17 +27,15 @@ export const projectData = [
         subtitle: '2D Game',
         date: '12/1/2022',
         paragraph: 'Developed a 2D JavaScript game using canvas. The JavaScript source code implements random generation of objects, hitbox buffers, and object-oriented programming.',
-        // paragraph: 'My inspiration for this project was to learn JavaScript and have fun doing it. Since I used to love playing video games, I decided to create a video game. The game is very simple: you control a Mars Rover and try to collect diamonds while avoiding incoming asteroids. Developing this game helped me apply object-oriented programming and data structures skills that I had learned in school, and it also got me into programming with the Reactjs framework which is very exciting.',
         hasYoutube: 'true',
         youtube: '8A9_LRPZJuQ',
         hasGithub: 'true',
-        githubSrc: 'https://github.com/chrisegnatis/resume/blob/main/Falling%20Asteriod%20Game.zip',
+        githubSrc: 'https://github.com/chrisegnatis/FallingAsteroid',
     },
     {
         title: 'Programming Calculator',
         date: '11/15/2021',
         paragraph: 'Programmed a project in MIPS to engineer the process of converting numbers from one base system to another. I was able to apply strong coding practices for assembly languages such as protecting the scope of variables, creating meaningful procedures, support for error handing and input variation, and strong documentation. The calculator supports up to 32-bit calculations (2s complement) and currently supports binary, decimal, and hexadecimal calculation.',
-        // paragraph: 'This project, which I created over a span of a couple weeks, demonstrates my ability in MIPS assembly language. I found this project challenging because it involves lots of complex procedures, calculations, and changing from character to number values: all in an assembly language. This project taught me how to make well-documented procedures, create consistent and modifiable code, and many other good programming practices. Contact me if you would like to see the actual source code.',
         hasYoutube: 'true',
         youtube: 'FZFPQjmJIvs',
         hasGithub: 'false'
@@ -46,8 +44,9 @@ export const projectData = [
         title: 'Geometry Helper',
         subtitle: 'Java Application',
         date: '5/1/2015',
-        paragraph: 'Jar Application created as a freshman in highschoool to help myself and others with rationalizing numbers. Implemented JFrame to display application.',
-        hasYoutube: 'false',
+        paragraph: 'Jar Application created as a freshman in highschoool to help myself and others with rationalizing numbers. Implemented JFrame to display application, programmed in java.',
+        hasYoutube: 'true',
+        youtube: 'TFbI2vqE3WU',
         hasGithub: 'true',
         githubSrc: 'https://github.com/chrisegnatis/GeometryHelper'
     }
